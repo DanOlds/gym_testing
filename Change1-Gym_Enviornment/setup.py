76c1af63d2c445eb1a9e969055e16ef7a0dfc9b2
Joshua Lynch <jlynch@bnl.gov>
2019-12-06 21:59:57 -0500

    add deep learning dependencies

--- Change1-Gym_Enviornment/setup.py
+++ Change1-Gym_Enviornment/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_change1',
       version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
+      install_requires=['gym', 'matplotlib', 'keras', 'tensorflow', 'tensorboard']
 )  
