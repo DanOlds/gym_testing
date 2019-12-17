d24a06a958a6bbcdc522da214bc1f86d387861a6
Olds <dolds@bnl.gov>
2019-12-17 10:05:27 -0500

    ENH Change2 gym

--- Change1-Gym_Enviornment/gym_change/envs/change1_env.py
+++ Change1-Gym_Enviornment/gym_change/envs/change1_env.py
@@ -50,14 +50,6 @@ class Change1(gym.Env):
         ##############
 
         self.seed()
-        
-        
-
-        #setup value-map
-        #self.x = np.arange(0,self.L,dtype=int)
-        #self.value_map = sigmoid(self.x,c=self.c,w=self.w)
-        #setup score-map
-        #self.score_map =d1_sigmoid(self.x,c=self.c,w=self.w)**self.power-self.sinkscore*max(d1_sigmoid(self.x,c=self.c,w=self.w)**self.power) 
 
         #state needs to begin with a bunch of nothing
         self.state = np.array((self.lookback+1)*[0.0, 0.0])
@@ -120,9 +112,136 @@ class Change1(gym.Env):
         
         self.power = power
         
-        #self.x = np.arange(0,self.L,dtype=int)
-        #self.score_map =d1_sigmoid(self.x,c=self.c,w=self.w)**self.power-self.sinkscore*max(d1_sigmoid(self.x,c=self.c,w=self.w)**self.power) 
-        #self.value_map = sigmoid(self.x,c=self.c,w=self.w)
+        #state needs to begin with a bunch of nothing
+        self.state = np.array((self.lookback+1)*[0.0, 0.0])
+        self.state[0] = 0
+        self.state[1] = self.value_map_func(self.state[0]) #first value
+        
+        #fill out first N=lookback spaces by taking single-steps
+        for _ in range(self.lookback):
+            self.step(0)
+        
+        return self.state
+
+class Change2(gym.Env):
+    """Model Change environment
+    This game presents moves along a linear chain of states, 
+    where each action is the number of states to take (from 1 to 10),
+    which moves along the chain.  The state will be returned as both the 
+    current integer-index, and the value of a scaler.  This
+    scaler will vary according to a sequence of sigmoid function, defined at the time of 
+    enviornment instantiation or reset (define width, center position, and amplitude).
+    
+    The reward from each point will be the derivative of this sigmoid function,
+    offset and rescaled by certain criteria passed to the env at creation.
+    
+    The goal is thus to take large jumps along the path when nothing is changing,
+    and small steps when changing dramatically.  The game completes when a move 
+    takes the state beyond end endpoint.
+
+    The observed state is the current state in the chain (0 to n-1) and value of scaler.
+    """    
+    def __init__(self, L=500, wmin=1, wmax=10, cmin=100, cmax=400, amin=-1,amax=1,
+                    sinkscore = 0.2, power=.50, lookback=4, num_flips = 3):
+        
+        self.num_flips = num_flips
+        self.L = L
+
+        self.cmin = cmin
+        self.cmax = cmax
+        self.wmin = wmin
+        self.wmax = wmax
+        self.amin = amin
+        self.amax = amax
+
+        self.clist = np.random.random(self.num_flips)*(self.cmax-self.cmin)+self.cmin
+        self.wlist = np.random.random(self.num_flips)*(self.wmax-self.wmin)+self.wmin
+        self.alist = np.random.random(self.num_flips)*(self.amax-self.amin)+self.amin
+        
+        self.power = power
+        self.sinkscore = sinkscore #fraction to drop score floor
+        self.lookback = lookback #how many previous entries to remember
+        
+        self.action_space = spaces.Discrete(10) #assume we can take steps 1 to 10
+
+        #setup observation_space
+        high = np.array((self.lookback+1)*[
+            int(self.L),
+            np.finfo(np.float32).max])
+        low = np.array((self.lookback+1)*[
+            int(0),
+            -1.0*np.finfo(np.float32).max])
+            
+        self.observation_space = spaces.Box(low, high, dtype=np.float32)
+        ##############
+
+        self.seed()
+        
+        
+        #state needs to begin with a bunch of nothing
+        self.state = np.array((self.lookback+1)*[0.0, 0.0])
+        self.state[0] = 0
+        self.state[1] = self.value_map_func(0) #first value
+
+        #####################
+        #fill out first N=lookback spaces by taking single-steps
+        for _ in range(self.lookback):
+            self.step(0)
+
+    def value_map_func(self,x):
+        my_func = 0*x
+            
+        for i in range(self.num_flips):
+            my_func += sigmoid(x,self.clist[i],self.wlist[i])*self.alist[i]
+        return my_func
+            
+    def score_map_func(self,x):
+        my_score = 0*x - self.sinkscore
+        for i in range(self.num_flips):
+            my_score += d1_sigmoid(x,self.clist[i],self.wlist[i])**self.power
+        return my_score
+    
+
+    def report(self):
+        print ("at site "+str((int(self.state[0]))) +" with value "+str(self.state[1]))
+        print ("back\tvalue")
+        for i in np.arange(2,2*self.lookback+1,2):
+            print (str(int(self.state[i]))+'\t'+str(self.state[i+1]))
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def step(self,action):
+        assert self.action_space.contains(action)
+        newstate = np.roll(self.state,2)
+        newstate[2] = 0
+        newstate[0] = 0
+        
+        for i in np.arange(2,int(self.lookback*2+1),2):
+            newstate[i] = newstate[i]+(action+1)
+            
+        newstate[0] = int(self.state[0])+(action+1)
+        
+        
+        if newstate[0] >= self.L:
+            done = True
+            newstate[0] = self.L-1
+        else:
+            done = False
+            
+        newstate[1] = self.value_map_func(newstate[0])
+        self.state = newstate
+        
+        reward = self.score_map_func(self.state[0])
+        
+        return self.state, reward, done, {}
+    
+    def reset(self):
+        
+        self.clist = np.random.random(self.num_flips)*(self.cmax-self.cmin)+self.cmin
+        self.wlist = np.random.random(self.num_flips)*(self.wmax-self.wmin)+self.wmin
+        self.alist = np.random.random(self.num_flips)*(self.amax-self.amin)+self.amin                
         
         #state needs to begin with a bunch of nothing
         self.state = np.array((self.lookback+1)*[0.0, 0.0])
--- run_keras_dqn.py
+++ run_keras_dqn.py
@@ -3,38 +3,56 @@ import gym
 import numpy as np
 from learn_change2 import DDQNAction
 import matplotlib.pyplot as plt
-from gym_change.envs.change1_env import Change1
+from gym_change.envs.change1_env import Change1, Change2
+import tensorflow as tf
+import datetime
 
 
 
 if __name__ == '__main__':
     #env = gym.make("CartPole-v0")
-    env = Change1(c=100, L=300,lookback=10,power=.25)
+    #env = Change2(lookback=10,power=.25,num_flips=3)
+    env = Change1(lookback=10,power=.50)
 
     ddqn_agent = DDQNAction(alpha = 0.0005, gamma = 0.99, n_actions = 10, epsilon = 1.0,
-                    batch_size = 64, input_dims=22)
+                    batch_size = 128, input_dims=22)
 
-    n_games = 1000
-    ddqn_agent.load_model() # if load a saved model
+    n_games = 2000
+    #ddqn_agent.load_model() # if load a saved model
     ddqn_scores = []
     eps_history = []
-    best_avg_score = -9999999.
-    best_single_score = -9999999.
+    reward_history = []
+    action_history = []
+
+    best_avg_score = -10.0
+    best_single_score = -10.0
     
+    STORE_PATH = './log/change'
+    train_writer = tf.summary.create_file_writer(STORE_PATH + \
+             f"/DoubleQ_{datetime.datetime.now().strftime('%d%m%Y%H%M')}")
+
     for i in range(n_games):
         done = False
         score = 0
-        observation = env.reset(cmin=50,cmax=250,wmin=4,wmax=6,power=.25)
+        action_counter = []
+        reward_counter = []
+        observation = env.reset()
         while not done:
             action = ddqn_agent.choose_action(observation)
             observation_, reward, done, info = env.step(action)
             score += reward
+            action_counter.append(action)
+            reward_counter.append(reward)
             ddqn_agent.remember(observation, action, reward, observation_, done)
             observation = observation_
             ddqn_agent.learn()
 
         eps_history.append(ddqn_agent.epsilon)
         ddqn_scores.append(score)
+        
+
+        #reward_history.append(reward_counter)
+        #action_history.append(action_counter)
 
         avg_score = np.mean(ddqn_scores[max(0, i-100):(i+1)])
         if avg_score > best_avg_score and i > 100:
@@ -47,9 +65,16 @@ if __name__ == '__main__':
             ddqn_agent.save_model('best_singlescore_dqn.h5')
             best_single_score = score
 
-
+        #print to screen stats
         print ('episode ', i, 'score %.2f' % score, 'average score %.2f' % avg_score)
 
+        #tensorboard output
+        with train_writer.as_default():
+            tf.summary.scalar('score', score, step=i)
+            tf.summary.scalar('avg_score', avg_score, step=i)
+            tf.summary.histogram('action distributions', action_counter, step=i)
+            tf.summary.histogram('reward distributions', reward_counter, step=i)
+        #periodically save agent
         if i % 10 == 0 and i > 0:
             ddqn_agent.save_model()
 
